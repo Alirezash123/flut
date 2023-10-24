f9cf5b4e72570f9873d6769db1627c33f3750104
Alireza <137312715+Alirezash123@users.noreply.github.com>
2023-10-24 09:59:46 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flut/micard.dart';
+import 'package:flut/poor.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:micard()
+    );
+  }
+}
